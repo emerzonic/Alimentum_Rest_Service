ff4a085337908a9a0032d1635457009ff2c1b19a
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-20 19:51:56 -0600

    security

--- src/main/java/alimentum/alimentum/AlimentumApplication.java
+++ src/main/java/alimentum/alimentum/AlimentumApplication.java
@@ -9,4 +9,15 @@ public class AlimentumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlimentumApplication.class, args);
 	}
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurerAdapter() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/user/**").allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
 }
+
