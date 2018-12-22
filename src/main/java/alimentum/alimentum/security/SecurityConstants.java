3758672c5505c86672668320d8adb66bad6c7cc2
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-22 15:40:59 -0600

    refactor security constants

--- src/main/java/alimentum/alimentum/security/SecurityConstants.java
+++ src/main/java/alimentum/alimentum/security/SecurityConstants.java
@@ -2,10 +2,11 @@ package alimentum.alimentum.security;
 
 public class SecurityConstants {
 
-  public static final String SIGN_UP_URLS = "/api/users/**";
-  public static final String H2_URL = "h2-console/**";
+  public static final String AUTH_URLS = "/api/users/**";
+  public static final String EXTERNAL_API_URLS = "/api/recipes/**";
+//  public static final String H2_URL = "h2-console/**";
   public static final String SECRET ="SecretKeyToGenJWTs";
   public static final String TOKEN_PREFIX= "Bearer ";
   public static final String HEADER_STRING = "Authorization";
-  public static final long EXPIRATION_TIME = 30_000; //30 seconds
+  public static final long EXPIRATION_TIME = 900_000_000; //30 seconds
 }
