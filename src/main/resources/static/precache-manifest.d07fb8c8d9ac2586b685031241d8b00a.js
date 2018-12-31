28c66a942786bccd8918f815bd1194cdc2a6d8bd
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-31 15:30:33 -0600

    deployment prep

---
+++ src/main/resources/static/precache-manifest.d07fb8c8d9ac2586b685031241d8b00a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ccbce86e27738e53d442c206226bf50",
+    "url": "/static/media/pasta.6ccbce86.jpg"
+  },
+  {
+    "revision": "8ae7ed7d0d5829f4df2a",
+    "url": "/static/css/main.05c5befe.chunk.css"
+  },
+  {
+    "revision": "8338d6ed01102006856d",
+    "url": "/static/js/1.8338d6ed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d82cdf17b89b9cfaaf37d966dedec09",
+    "url": "/static/media/breakfast.8d82cdf1.jpg"
+  },
+  {
+    "revision": "87f144924c544b9566d4085ab7c5fecf",
+    "url": "/static/media/chicken.87f14492.jpg"
+  },
+  {
+    "revision": "2ac81dac88027e2ef0bcfc98a33f8896",
+    "url": "/static/media/desert.2ac81dac.jpg"
+  },
+  {
+    "revision": "780446446c0aa94442a5ed58c8b70756",
+    "url": "/static/media/beef.78044644.jpg"
+  },
+  {
+    "revision": "9d2e92f0811909e4683ed903491ee481",
+    "url": "/static/media/vege.9d2e92f0.jpg"
+  },
+  {
+    "revision": "2849a26c5ef2d07fc685d509aba6c561",
+    "url": "/static/media/lamb.2849a26c.jpg"
+  },
+  {
+    "revision": "8ae7ed7d0d5829f4df2a",
+    "url": "/static/js/main.8ae7ed7d.chunk.js"
+  },
+  {
+    "revision": "34cedc0f45cf6d2184442c204994a0d0",
+    "url": "/static/media/pork.34cedc0f.jpg"
+  },
+  {
+    "revision": "38664d9ecf8e0c845cf664a5535ed4ff",
+    "url": "/static/media/seafood.38664d9e.jpg"
+  },
+  {
+    "revision": "cd55669c8c27082cad046e4d2fb0b6b1",
+    "url": "/static/media/side.cd55669c.jpg"
+  },
+  {
+    "revision": "61b89395f5780d8c110e428f27166f65",
+    "url": "/static/media/starter.61b89395.jpg"
+  },
+  {
+    "revision": "6956954ae8c20c0054b71afd0fd20d9d",
+    "url": "/static/media/vegan.6956954a.jpg"
+  },
+  {
+    "revision": "e40c926f36f9855e0555b33ff36a4514",
+    "url": "/static/media/headerImg.e40c926f.png"
+  },
+  {
+    "revision": "ff6537c12848d80de31b79524333c412",
+    "url": "/static/media/error2.ff6537c1.gif"
+  },
+  {
+    "revision": "e55ad030a42c960ea32819163cfc12da",
+    "url": "/static/media/youtube.e55ad030.jpg"
+  },
+  {
+    "revision": "e3d0b5e8b8dd561ce5e410c04d3755c9",
+    "url": "/index.html"
+  }
+];
