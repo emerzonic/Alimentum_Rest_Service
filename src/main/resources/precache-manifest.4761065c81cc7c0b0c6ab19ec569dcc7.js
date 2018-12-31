e558bae07a3b9b12a53bc9f688bdcc9fa5fa7a67
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-31 13:19:38 -0600

    added react static files

---
+++ src/main/resources/precache-manifest.4761065c81cc7c0b0c6ab19ec569dcc7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ccbce86e27738e53d442c206226bf50",
+    "url": "/Alimentum-version-2.0/static/media/pasta.6ccbce86.jpg"
+  },
+  {
+    "revision": "83455bf9d99b715cded0",
+    "url": "/Alimentum-version-2.0/static/css/main.05c5befe.chunk.css"
+  },
+  {
+    "revision": "8338d6ed01102006856d",
+    "url": "/Alimentum-version-2.0/static/js/1.8338d6ed.chunk.js"
+  },
+  {
+    "revision": "3e235a859211cc87b1bc",
+    "url": "/Alimentum-version-2.0/static/js/runtime~main.3e235a85.js"
+  },
+  {
+    "revision": "8d82cdf17b89b9cfaaf37d966dedec09",
+    "url": "/Alimentum-version-2.0/static/media/breakfast.8d82cdf1.jpg"
+  },
+  {
+    "revision": "87f144924c544b9566d4085ab7c5fecf",
+    "url": "/Alimentum-version-2.0/static/media/chicken.87f14492.jpg"
+  },
+  {
+    "revision": "2ac81dac88027e2ef0bcfc98a33f8896",
+    "url": "/Alimentum-version-2.0/static/media/desert.2ac81dac.jpg"
+  },
+  {
+    "revision": "780446446c0aa94442a5ed58c8b70756",
+    "url": "/Alimentum-version-2.0/static/media/beef.78044644.jpg"
+  },
+  {
+    "revision": "9d2e92f0811909e4683ed903491ee481",
+    "url": "/Alimentum-version-2.0/static/media/vege.9d2e92f0.jpg"
+  },
+  {
+    "revision": "2849a26c5ef2d07fc685d509aba6c561",
+    "url": "/Alimentum-version-2.0/static/media/lamb.2849a26c.jpg"
+  },
+  {
+    "revision": "83455bf9d99b715cded0",
+    "url": "/Alimentum-version-2.0/static/js/main.83455bf9.chunk.js"
+  },
+  {
+    "revision": "34cedc0f45cf6d2184442c204994a0d0",
+    "url": "/Alimentum-version-2.0/static/media/pork.34cedc0f.jpg"
+  },
+  {
+    "revision": "38664d9ecf8e0c845cf664a5535ed4ff",
+    "url": "/Alimentum-version-2.0/static/media/seafood.38664d9e.jpg"
+  },
+  {
+    "revision": "cd55669c8c27082cad046e4d2fb0b6b1",
+    "url": "/Alimentum-version-2.0/static/media/side.cd55669c.jpg"
+  },
+  {
+    "revision": "61b89395f5780d8c110e428f27166f65",
+    "url": "/Alimentum-version-2.0/static/media/starter.61b89395.jpg"
+  },
+  {
+    "revision": "6956954ae8c20c0054b71afd0fd20d9d",
+    "url": "/Alimentum-version-2.0/static/media/vegan.6956954a.jpg"
+  },
+  {
+    "revision": "e40c926f36f9855e0555b33ff36a4514",
+    "url": "/Alimentum-version-2.0/static/media/headerImg.e40c926f.png"
+  },
+  {
+    "revision": "ff6537c12848d80de31b79524333c412",
+    "url": "/Alimentum-version-2.0/static/media/error2.ff6537c1.gif"
+  },
+  {
+    "revision": "e55ad030a42c960ea32819163cfc12da",
+    "url": "/Alimentum-version-2.0/static/media/youtube.e55ad030.jpg"
+  },
+  {
+    "revision": "36d6ec4c55888bb04e7598d33cdcfd3c",
+    "url": "/Alimentum-version-2.0/index.html"
+  }
+];
