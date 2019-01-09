aac9af928c540077b5f5a5234871c150815e4fa0
Emerson Doyah <emerson.doyah@gmail.com>
2019-01-09 17:00:13 -0600

    updated crossorigin

--- src/main/java/alimentum/alimentum/controller/RecipeController.java
+++ src/main/java/alimentum/alimentum/controller/RecipeController.java
@@ -18,7 +18,7 @@ import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "https://alimentum-recipe-finder.herokuapp.com/")
+@CrossOrigin(origins = "https://alimentum-recipe-finder.herokuapp.com/, https://calm-spire-56233.herokuapp.com/")
 @RestController
 public class RecipeController {
 
--- src/main/java/alimentum/alimentum/controller/UserController.java
+++ src/main/java/alimentum/alimentum/controller/UserController.java
@@ -22,7 +22,7 @@ import javax.validation.Valid;
 
 import static alimentum.alimentum.security.SecurityConstants.TOKEN_PREFIX;
 
-@CrossOrigin(origins = "https://alimentum-recipe-finder.herokuapp.com/")
+@CrossOrigin(origins = "https://alimentum-recipe-finder.herokuapp.com/, https://calm-spire-56233.herokuapp.com/")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
